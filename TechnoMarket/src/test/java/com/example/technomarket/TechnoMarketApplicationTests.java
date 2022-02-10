6e8dcbfb517d521c9a023106326f5cac3adcd5cd
Ivan Atanasov <atanasov.ivang@gmail.com>
2022-02-10 20:16:23 +0200

    Creating Spring Project

---
+++ TechnoMarket/src/test/java/com/example/technomarket/TechnoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnoMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
