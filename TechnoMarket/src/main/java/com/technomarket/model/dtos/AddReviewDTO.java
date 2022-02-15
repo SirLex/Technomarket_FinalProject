57f5d4070cdd2550a36363ee8e85667071c2903f
Ivan Atanasov <atanasov.ivang@gmail.com>
2022-02-15 16:26:14 +0200

    Added AddReviewDTO

---
+++ TechnoMarket/src/main/java/com/technomarket/model/dtos/AddReviewDTO.java
@@ -0,0 +1,37 @@
+package com.technomarket.model.dtos;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddReviewDTO {
+
+    @NotBlank
+    private String title;
+
+    @NotNull
+    private String alias;
+
+    @NotNull
+    @Length(min = 10)
+    private String text;
+
+    @NotNull
+    @Min(1)
+    @Max(5)
+    private int rating;
+
+}
