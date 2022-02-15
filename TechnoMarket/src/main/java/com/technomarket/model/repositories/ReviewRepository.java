cb683ca8a3498e0c3ec7c3a052cdfebff54e6b75
Ivan Atanasov <atanasov.ivang@gmail.com>
2022-02-15 18:33:36 +0200

    Added ReviewRepository

---
+++ TechnoMarket/src/main/java/com/technomarket/model/repositories/ReviewRepository.java
@@ -0,0 +1,16 @@
+package com.technomarket.model.repositories;
+
+
+import com.technomarket.model.pojos.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findAllByProductId(Long productId);
+
+    List<Review> findAllByUserId(Long userId);
+}
