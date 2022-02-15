dfaeca3518ca4fb7d106b5802dd979d9764ee4ce
Ivan Atanasov <atanasov.ivang@gmail.com>
2022-02-15 17:04:36 +0200

    Added EditProductDTO

--- TechnoMarket/src/main/java/com/technomarket/model/dtos/CategoryDTO.java
+++ TechnoMarket/src/main/java/com/technomarket/model/dtos/CategoryDTO.java
@@ -2,7 +2,7 @@ package com.technomarket.model.dtos;
 
 public class CategoryDTO {
 
-    
+
     private Long id;
 
     private String name;
---
+++ TechnoMarket/src/main/java/com/technomarket/model/dtos/EditProductDTO.java
@@ -0,0 +1,37 @@
+package com.technomarket.model.dtos;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditProductDTO {
+
+    @NotNull
+    private long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @Min(0)
+    private BigDecimal price;
+
+    @NotNull
+    @Min(0)
+    private int discount;
+
+}
