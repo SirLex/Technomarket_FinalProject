d63613fb34b66e09ea0e6cd15fd3b62a8e8714da
Ivan Atanasov <atanasov.ivang@gmail.com>
2022-02-15 18:26:20 +0200

    Added ProductDTO

---
+++ TechnoMarket/src/main/java/com/technomarket/model/dtos/ProductDTO.java
@@ -0,0 +1,34 @@
+package com.technomarket.model.dtos;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProductDTO {
+
+    private long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private BigDecimal price;
+
+    private Integer discount;
+    
+    private Long subCategoryId;
+}
