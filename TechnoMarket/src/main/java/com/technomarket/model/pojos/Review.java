f71bb8872e1d2a1e7a27e9cd7061023366c23e6a
Ivan Atanasov <atanasov.ivang@gmail.com>
2022-02-16 19:59:08 +0200

    Added Review Pojo

--- TechnoMarket/src/main/java/com/technomarket/model/pojos/Review.java
+++ TechnoMarket/src/main/java/com/technomarket/model/pojos/Review.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "reviews")
@@ -16,14 +17,23 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column
+
+    @Column(name = "userId", nullable = false)
     private int userId;
-    @Column
+
+    @Column(name = "productId", nullable = false)
     private int productId;
-    @Column
+
+    @Column(name = "rating", nullable = false)
     private int rating;
-    @Column
+
+
+    @Column(name = "comment", nullable = false)
     private String comment;
-    @Column
+
+    @Column(name="isRecommended")
     private boolean isRecommended;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
 }
