33e5478e66c6c61e996a67a22576b65a3e85114a
Alex Kolachev <xxglentxx@gmail.com>
2022-02-16 19:48:47 +0200

    Actually resolving conflict

--- TechnoMarket/src/main/java/com/technomarket/model/repositories/UserRepository.java
+++ TechnoMarket/src/main/java/com/technomarket/model/repositories/UserRepository.java
@@ -6,12 +6,7 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-<<<<<<< HEAD
 public interface UserRepository extends JpaRepository<User, Integer> {
-=======
-public interface UserRepository extends JpaRepository<User, Integer> {
->>>>>>> efebfe6ac13aa2ed33888ef2dea8a39a36d3b1f9
-
     boolean existsByEmail(String email);
     User findByEmail(String email);
 }
