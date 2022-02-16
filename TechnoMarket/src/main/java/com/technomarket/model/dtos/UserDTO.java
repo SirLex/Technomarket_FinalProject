174cabc1751f861e16356fd31a9e5c14e1bc0b3b
Ivan Atanasov <atanasov.ivang@gmail.com>
2022-02-16 18:33:12 +0200

    Added UserDTO

---
+++ TechnoMarket/src/main/java/com/technomarket/model/dtos/UserDTO.java
@@ -0,0 +1,23 @@
+package com.technomarket.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserDTO {
+
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
