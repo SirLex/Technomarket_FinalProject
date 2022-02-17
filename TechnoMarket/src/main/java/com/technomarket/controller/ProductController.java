0a6172277ae1d68082148403aa08bf2b6e1c236d
Alex Kolachev <xxglentxx@gmail.com>
2022-02-17 21:40:50 +0200

    Added ProductController

--- TechnoMarket/src/main/java/com/technomarket/controller/ProductController.java
+++ TechnoMarket/src/main/java/com/technomarket/controller/ProductController.java
@@ -2,12 +2,17 @@ package com.technomarket.controller;
 
 
 import com.technomarket.model.dtos.MessageDTO;
-import com.technomarket.model.dtos.product.ProductRequestDTO;
+import com.technomarket.model.dtos.product.ProductAddDTO;
 import com.technomarket.model.dtos.product.ProductResponseDTO;
+import com.technomarket.model.pojos.Product;
 import com.technomarket.model.services.ProductService;
+import com.technomarket.model.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
@@ -16,21 +21,25 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-/*
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/product")
+    public ResponseEntity<ProductResponseDTO> addProduct(@Valid @RequestBody ProductAddDTO productDTO, HttpServletRequest request) {
+        UserController.validateLogin(request);
+        HttpSession session = request.getSession();
+        int userId = (int) session.getAttribute(UserController.USER_ID);
+        userService.adminValidation(userId);
+        //TODO add attributes
+        ProductResponseDTO response = productService.addProduct(productDTO);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 
     @GetMapping("/product/{id}")
-    public ProductResponseDTO getById(@PathVariable int id){
-        ProductResponseDTO responseDTO = productService.getById(id);
+    public Product getById(@PathVariable int id) {
+        return productService.getById(id);
     }
-
-    @PutMapping("/add/product")
-    public ProductResponseDTO addProduct(@Valid @RequestBody ProductRequestDTO productDTO, HttpSession session){
-    }
-
-    @DeleteMapping("/product/{id}")
-    public MessageDTO deleteProduct(@PathVariable int id, HttpSession session){
-
-    }
-*/
 
 }
