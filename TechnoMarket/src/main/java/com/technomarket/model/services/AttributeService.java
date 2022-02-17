e072efcb1a16a091fffe1241ff94d3f9143af379
Alex Kolachev <xxglentxx@gmail.com>
2022-02-17 19:04:56 +0200

    Added AttributeService

---
+++ TechnoMarket/src/main/java/com/technomarket/model/services/AttributeService.java
@@ -0,0 +1,33 @@
+package com.technomarket.model.services;
+
+import com.technomarket.exceptions.BadRequestException;
+import com.technomarket.model.dtos.attribute.AttributeAddDTO;
+import com.technomarket.model.pojos.Attributes;
+import com.technomarket.model.repositories.AttributeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AttributeService {
+
+    @Autowired
+    private AttributeRepository attributeRepository;
+
+    public Attributes addAttribute(AttributeAddDTO attributeDTO) {
+        if(attributeRepository.existsByName(attributeDTO.getName())){
+            throw new BadRequestException("There is already attribute with that name");
+        }
+        Attributes attribute = new Attributes();
+        attribute.setName(attributeDTO.getName());
+        attributeRepository.save(attribute);
+        return attribute;
+    }
+
+    public Attributes getById(int id) {
+        if(!attributeRepository.existsById(id)){
+            throw new BadRequestException("There is no attribute with this id");
+        }
+        return attributeRepository.getById(id);
+    }
+}
